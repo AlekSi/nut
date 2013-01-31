1766fb333988cc6efc818b4e60cb41be7bcdcb98
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2013-01-31 23:43:31 +0400

    Add test.

--- nut_test.go
+++ nut_test.go
@@ -2,9 +2,11 @@ package nut_test
 
 import (
 	"bytes"
+	"fmt"
 	"io/ioutil"
 	"os"
 	"path/filepath"
+	"runtime"
 
 	. "."
 	. "launchpad.net/gocheck"
@@ -43,12 +45,13 @@ func (f *N) TestNutFile(c *C) {
 	c.Check(nf.FilePath("prefix"), Equals, filepath.FromSlash("prefix/test_nut1-0.0.1.nut"))
 	c.Check(nf.ImportPath("prefix"), Equals, "prefix/test_nut1/0.0.1")
 	c.Check(nf.Doc, Equals, "Package test_nut1 is used to test nut.")
-	c.Check(nf.GoFiles, DeepEquals, []string{"test_nut1.go", "test_nut11.go"})
+	c.Check(nf.GoFiles, DeepEquals, []string{"test_nut1.go", fmt.Sprintf("test_nut1_%s.go", runtime.GOOS)})
 
-	c.Check(len(nf.Reader.File), Equals, 5)
+	c.Check(len(nf.Reader.File), Equals, 6)
 	names := make([]string, 0, 4)
 	for _, f := range nf.Reader.File {
 		names = append(names, f.Name)
 	}
-	c.Check([]string{"test_nut1.go", "test_nut11.go", "README", "LICENSE", "nut.json"}, DeepEquals, names)
+	c.Check([]string{"test_nut1.go", "test_nut1_darwin.go", "test_nut1_linux.go", "README", "LICENSE", "nut.json"},
+		DeepEquals, names)
 }
