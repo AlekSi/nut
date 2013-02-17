58d5f3e5344eb21734fbded933bfe8f54da32d37
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2013-02-17 15:51:35 +0400

    Add Windows batch file.

---
+++ build.bat
@@ -0,0 +1,16 @@
+go build -o gonut.exe github.com/AlekSi/nut/nut
+@if ERRORLEVEL 1 exit /B %ERRORLEVEL%
+
+@pushd ..\test_nut1
+..\nut\gonut.exe pack
+@if ERRORLEVEL 1 exit /B %ERRORLEVEL%
+@popd
+
+go test -v github.com/AlekSi/nut -gocheck.v
+@if ERRORLEVEL 1 exit /B %ERRORLEVEL%
+
+go test -v github.com/AlekSi/nut/nut -gocheck.v
+@if ERRORLEVEL 1 exit /B %ERRORLEVEL%
+
+go test -v -short github.com/AlekSi/nut/integration_test -gocheck.v
+@if ERRORLEVEL 1 exit /B %ERRORLEVEL%
