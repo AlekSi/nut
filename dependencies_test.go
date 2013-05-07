c7764fd83146cb7340d83012b1fca9d3d61a1182
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2013-05-07 19:50:48 +0400

    Tweak tests.

--- dependencies_test.go
+++ dependencies_test.go
@@ -5,24 +5,29 @@ import (
 	. "launchpad.net/gocheck"
 )
 
-type Ds struct{}
+type Ds struct {
+	deps *Dependencies
+}
 
 var _ = Suite(&Ds{})
 
-func (*Ds) TestDependenciesAdd(c *C) {
-	deps := NewDependencies()
+func (ds *Ds) SetUpTest(c *C) {
+	ds.deps = NewDependencies()
+}
+
+func (ds *Ds) TestDependenciesAdd(c *C) {
 	for _, v := range []string{"1.>=1.*", "1.>=2.*", "1.*.*"} {
 		dep, err := NewDependency("gonuts.io/debug/crazy", v)
 		c.Check(err, IsNil)
-		err = deps.Add(dep)
+		err = ds.deps.Add(dep)
 		c.Check(err, IsNil)
 	}
-	c.Check(deps.Get("gonuts.io/debug/crazy").String(), Equals, "gonuts.io/debug/crazy (1.>=2.*)")
+	c.Check(ds.deps.Get("gonuts.io/debug/crazy").String(), Equals, "gonuts.io/debug/crazy (1.>=2.*)")
 
 	dep, err := NewDependency("gonuts.io/debug/crazy", "2.*.*")
 	c.Check(err, IsNil)
-	err = deps.Add(dep)
+	err = ds.deps.Add(dep)
 	c.Check(err, FitsTypeOf, &AddDependencyError{})
 	c.Check(err.Error(), Equals, "Can't add gonuts.io/debug/crazy (2.*.*) to existing dependecy gonuts.io/debug/crazy (1.>=2.*)")
-	c.Check(deps.Get("gonuts.io/debug/crazy").String(), Equals, "gonuts.io/debug/crazy (1.>=2.*)")
+	c.Check(ds.deps.Get("gonuts.io/debug/crazy").String(), Equals, "gonuts.io/debug/crazy (1.>=2.*)")
 }
